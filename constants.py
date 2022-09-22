7a8d16ecbf3013226f1e9f7bd867ed64a7f70020
Richard <richardnky@hotmail.com>
2022-09-22 11:00:42 +0800

    updated new deployed contracts

--- constants.py
+++ constants.py
@@ -6,23 +6,23 @@ avax_url="https://api.avax-test.network/ext/bc/C/rpc"
 wallet_address = "0x6Ec43b644cD769a1c7695069804E67b2B691e36d"
 
 # Baklava client
-synthetic_pool_address="0x1f2A2A8eBF8Ec7102Bf15cb6eC5629A9E05b410a"
+synthetic_pool_address="0xdCE353cf62250B94C80F8eD70FFe5fF95304226f"
 ABI_synthetic_pool = json.load(open(os.getcwd()+'/BaklavaClient/assets/'+'SyntheticPool.json'))["abi"]
 
 pair_info = {
     0:{
         "pair":"TSLA:USDT",
-        "address":"0x0D95b3f47606339FE7055938e1fACc457177aE21",
+        "address":"0xfA0d3DDbcfe67D66d8BAA7f963ccDd5551ee19Bf",
         "chain_id":"tsla"
         },
     1:{
         "pair":"AAPL:USDT",
-        "address":"0x9BD0E966D7457810862E57c8F1e36a1c331fEca0",
+        "address":"0x01a98b5eF7D34b5e60dbF89cff73b6f6aE5162DD",
         "chain_id":"aapl"
     },
     2:{
         "pair":"BTC:USDT",
-        "address":"0xA2c2c0686FabEd8186E29CeBeB7cccBC416cb03D",
+        "address":"0x33a922c23Fe61b6D05AC2De88b7714809c0faD44",
         "chain_id":"btc"
     },
     # 3:{
